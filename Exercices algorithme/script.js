6e6babf7a136bcef15932ae9e8c4adfb25124bc5
Tribak77 <atribak34@gmail.com>
2024-09-10 10:08:39 +0100

    Bubble Sort

--- Exercices algorithme/script.js
+++ Exercices algorithme/script.js
@@ -1,13 +1,29 @@
-let numbers = [4, 2, 0, 9, -7];
+//Selection Sort
+let numbers1 = [4, 2, 0, 9, -7];
 
-let temps;
-for (let i = 0; i < numbers.length; i++) {
-    for (let j = i + 1; j < numbers.length; j++) {
-        if (numbers[i] > numbers[j]) {
-            temps = numbers[i];
-            numbers[i] = numbers[j];
-            numbers[j] = temps;
+let temps1;
+for (let i = 0; i < numbers1.length; i++) {
+    for (let j = i + 1; j < numbers1.length; j++) {
+        if (numbers1[i] > numbers1[j]) {
+            temps1 = numbers1[i];
+            numbers1[i] = numbers1[j];
+            numbers1[j] = temps1;
         }
     }
 }
-console.log(numbers);
+console.log(numbers1);
+
+//Bubble Sort
+let numbers2 = [4, 1, 0, 8, -7];
+let temps2;
+for(let i=0; i<numbers2.length; i++){
+for(let j=0; j<numbers2.length;j++){
+    if(numbers2[j]>numbers2[j+1]){
+        temps2 = numbers2[j];
+        numbers2[j] = numbers2[j+1];
+        numbers2[j+1] = temps2;
+    }
+}
+}
+console.log(numbers2);
+
