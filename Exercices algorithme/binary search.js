9842ec925fa26ef4ef7f0eccc81bc9fffcae6ca7
Tribak77 <atribak34@gmail.com>
2024-09-10 15:06:04 +0100

    binary search

--- Exercices algorithme/binary search.js
+++ Exercices algorithme/binary search.js
@@ -1,12 +1,12 @@
 let array = [1, 2, 3, 4, 5, 6, 7, 8];
-let target = 3;
+let target = 5;
 let left = 0;
 let right = array.length - 1;
 
 while (left <= right) {
     let middel = Math.floor((left + right) / 2);
     if (array[middel] === target) {
-        console.log(middel);
+        console.log("the index of ",target," is: ",middel);
     }
     if (array[middel] < target) {
         left =middel;
